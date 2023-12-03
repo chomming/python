2efd3ccdfc9117a7c1c95b4e7223783c8cf6d924
chomming <81208053+chomming@users.noreply.github.com>
2023-12-03 18:02:31 +0900

    Update 10818.py

--- baekjoon/10818.py
+++ baekjoon/10818.py
@@ -1,1 +1,12 @@
+N = int(input())
+T = list(map(int, input().split()))
+max = T[0]
+min = T[0]
 
+for i in range(0, len(T)):
+    if max < T[i]:
+        max = T[i]
+    elif min > T[i]:
+        min = T[i]
+
+print(min, max)
